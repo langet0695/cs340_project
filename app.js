9e137833dbf58c723c7cfdb1ac85d30b723bfe1d
dmeitho3 <91294471+meithofdOSU@users.noreply.github.com>
2022-03-09 20:43:45 -0800

    updated

--- app.js
+++ app.js
@@ -44,6 +44,242 @@ function getAndRenderCustomers(req, res)
         }
     });
 }
+
+// update Customer
+function updateCustomer(req, res)
+{
+    var columnUpdates = []
+    if(req.query['include_email'] && req.query.include_email == 'yes')
+    {
+        columnUpdates.push(`email = "${req.query['email']}"`);
+    }
+    if(req.query['include_street'] && req.query.include_street == 'yes')
+    {
+        columnUpdates.push(`street = "${req.query['street']}"`);
+    }
+    if(req.query['include_city'] && req.query.include_city == 'yes')
+    {
+        columnUpdates.push(`city = "${req.query['city']}"`);
+    }
+    if(req.query['include_zip'] && req.query.include_zip == 'yes')
+    {
+        columnUpdates.push(`zip = "${req.query['zip']}"`);
+    }
+    if(req.query['status'] && (req.query.status == '0' || req.query.status == '1'))
+    {
+        columnUpdates.push(`status = ${req.query['status']}`);
+    }
+    if(req.query['lastPurchaseDate'])
+    {
+        columnUpdates.push(`lastPurchaseDate = "${req.query['lastPurchaseDate']}"`);
+    }
+    if(req.query['lastPurchaseID'])
+    {
+        columnUpdates.push(`lastPurchaseID = ${req.query['lastPurchaseID']}`);
+    }
+
+    if (columnUpdates.length > 0)
+    {
+        var sql = "UPDATE Customers SET " + columnUpdates.join(", ") + ` WHERE customerID = ${req.query['customerID']};`
+
+        console.log(sql);
+    
+        db.pool.query(sql, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                getAndRenderCustomers(req, res);
+            }
+        });
+    }
+    else
+    {
+        // Nothing to update because the client supplied no fields to update.
+        // We need to display an error message to the client.
+        console.error("Unable to update Customers: No data to update");
+        // For now, just render the page without incident.
+        getAndRenderCustomers(req, res);
+    }
+}
+
+// update orderContent
+function updateorderContent(req, res)
+{
+    var columnUpdates = []
+    if(req.query['include_oid'] && req.query.include_oid == 'yes')
+    {
+        columnUpdates.push(`orderID = ${req.query['oid']}`);
+    }
+    if(req.query['include_pid'] && req.query.include_pid == 'yes')
+    {
+        columnUpdates.push(`productID = ${req.query['pid']}`);
+    }
+    if(req.query['quantityOrdered'])
+    {
+        columnUpdates.push(`quantityOrdered = ${req.query['quantityOrdered']}`);
+    }
+
+    if (columnUpdates.length > 0)
+    {
+        var sql = "UPDATE OrderContents SET " + columnUpdates.join(", ") + ` WHERE contentID = ${req.query['cid']};`
+
+        console.log(sql);
+    
+        db.pool.query(sql, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                getAndRenderorderContents(req, res);
+            }
+        });
+    }
+    else
+    {
+        // Nothing to update because the client supplied no fields to update.
+        // We need to display an error message to the client.
+        console.error("Unable to update Orders: No data to update");
+        // For now, just render the page without incident.
+        getAndRenderorderContents(req, res);
+    }
+}
+// update products
+function updateProducts(req, res)
+{
+    var columnUpdates = []
+    if(req.query['include_promoID'] && req.query.include_promoID == 'yes')
+    {
+        columnUpdates.push(`promoID = ${req.query['promoID']}`);
+    }
+    if(req.query['include_productType'] && req.query.include_productType == 'yes')
+    {
+        columnUpdates.push(`productType = "${req.query['productType']}"`);
+    }
+    if(req.query['include_description'] && req.query.description == 'yes')
+    {
+        columnUpdates.push(`description = "${req.query['description']}"`);
+    }
+    if(req.query['price'])
+    {
+        columnUpdates.push(`price = ${req.query['price']}`);
+    }
+    if(req.query['quantityInStock'])
+    {
+        columnUpdates.push(`quantityInStock = ${req.query['quantityInStock']}`);
+    }
+
+    if (columnUpdates.length > 0)
+    {
+        var sql = "UPDATE PlantsUnlimitedProducts SET " + columnUpdates.join(", ") + ` WHERE productID = ${req.query['productID']};`
+
+        console.log(sql);
+    
+        db.pool.query(sql, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                getAndRenderproducts(req, res);
+            }
+        });
+    }
+    else
+    {
+        // Nothing to update because the client supplied no fields to update.
+        // We need to display an error message to the client.
+        console.error("Unable to update PlantsUnlimitedProducts: No data to update");
+        // For now, just render the page without incident.
+        getAndRenderproducts(req, res);
+    }
+}
+// update promotions 
+function updatePromotions(req, res)
+{
+    var columnUpdates = []
+    if(req.query['status'] && (req.query.status == '0' || req.query.status == '1'))
+    {
+        columnUpdates.push(`status = ${req.query['status']}`);
+    }
+    if(req.query['discountSize'])
+    {
+        columnUpdates.push(`discountSize = ${req.query['discountSize']}`);
+    }
+
+    if (columnUpdates.length > 0)
+    {
+        var sql = "UPDATE Promotions SET " + columnUpdates.join(", ") + ` WHERE promoID = ${req.query['promoID']};`
+
+        console.log(sql);
+    
+        db.pool.query(sql, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                getAndRenderpromotions(req, res);
+            }
+        });
+    }
+    else
+    {
+        // Nothing to update because the client supplied no fields to update.
+        // We need to display an error message to the client.
+        console.error("Unable to update Promotions: No data to update");
+        // For now, just render the page without incident.
+        getAndRenderpromotions(req, res);
+    }
+}
+
+function updateSales(req, res)
+{
+    var columnUpdates = []
+    if(req.query['include_cid'] && req.query.include_cid == 'yes')
+    {
+        columnUpdates.push(`customerID = ${req.query['customerID']}`);
+    }
+    if(req.query['saleDate'])
+    {
+        columnUpdates.push(`saleDate = "${req.query['saleDate']}"`);
+    }
+    if(req.query['orderFulfilled'] && (req.query.orderFulfilled == '0' || req.query.orderFulfilled == '1'))
+    {
+        columnUpdates.push(`orderFulfilled = ${req.query['orderFulfilled']}`);
+    }
+    if(req.query['orderFulfilledDate'])
+    {
+        columnUpdates.push(`orderFulfilledDate = "${req.query['orderFulfilledDate']}"`);
+    }
+    if(req.query['totalPrice'])
+    {
+        columnUpdates.push(`totalPrice = ${req.query['totalPrice']}`);
+    }
+
+    if (columnUpdates.length > 0)
+    {
+        var sql = "UPDATE Sales SET " + columnUpdates.join(", ") + ` WHERE orderID = ${req.query['orderID']};`
+
+        console.log(sql);
+    
+        db.pool.query(sql, function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                getAndRendersales(req, res);
+            }
+        });
+    }
+    else
+    {
+        // Nothing to update because the client supplied no fields to update.
+        // We need to display an error message to the client.
+        console.error("Unable to update Sales: No data to update");
+        // For now, just render the page without incident.
+        getAndRendersales(req, res);
+    }
+}
+
 function getAndRenderorderContents(req, res)
 {
     db.pool.query("SELECT * FROM OrderContents;", function(error, results, fields) {
@@ -163,6 +399,19 @@ app.get('/customers', function(req, res)
                 getAndRenderCustomers(req, res);
             }
         });
+    } else if (req.query["crud"] && req.query.crud == 'update') {
+        var customerId = req.query['customerID'];
+        if (customerId)
+        {
+            updateCustomer(req, res);
+        }
+        else
+        {
+            // Output an error of some kind.
+            console.error("Missing customer ID for update");
+            // For now, just render the page and quietly ignore.
+            getAndRenderCustomers(req, res);
+        }
     } else if (req.query["crud"] && req.query.crud == 'delete') {
         // Delete using email as the identifier
         let sql = `Delete From Customers Where email = ${req.query.email};`
@@ -200,6 +449,19 @@ app.get('/promotions', function(req, res)
                 getAndRenderpromotions(req, res);
             }
         });
+    } else if (req.query["crud"] && req.query.crud == 'update') {
+        var promoID = req.query['promoID'];
+        if (promoID)
+        {
+            updatePromotions(req, res);
+        }
+        else
+        {
+            // Output an error of some kind.
+            console.error("Missing promo ID for update");
+            // For now, just render the page and quietly ignore.
+            getAndRenderpromotions(req, res);
+        }
     } else if (req.query["crud"] && req.query.crud == 'delete') {
         // Delete using promoId as the identifier
         let sql = `Delete From Promotions Where promoID = ${req.query.promoID};`
@@ -237,6 +499,19 @@ app.get('/sales', function(req, res)
                 getAndRendersales(req, res);
             }
         });
+    } else if (req.query["crud"] && req.query.crud == 'update') {
+        var orderID = req.query['orderID'];
+        if (orderID)
+        {
+            updateSales(req, res);
+        }
+        else
+        {
+            // Output an error of some kind.
+            console.error("Missing order ID for update");
+            // For now, just render the page and quietly ignore.
+            getAndRendersales(req, res);
+        }
     } else if (req.query["crud"] && req.query.crud == 'delete') {
         // Delete using orderId as the identifier
         let sql = `Delete From Sales Where orderID= ${req.query.orderID};`
@@ -274,6 +549,19 @@ app.get('/products', function(req, res)
                 getAndRenderproducts(req, res);
             }
         });
+    } else if (req.query["crud"] && req.query.crud == 'update') {
+        var productID = req.query['productID'];
+        if (productID)
+        {
+            updateProducts(req, res);
+        }
+        else
+        {
+            // Output an error of some kind.
+            console.error("Missing product ID for update");
+            // For now, just render the page and quietly ignore.
+            getAndRenderproducts(req, res);
+        }
     } else if (req.query["crud"] && req.query.crud == 'delete') {
         // Delete using productType as the identifier
         let sql = `Delete From PlantsUnlimitedProducts Where productType = ${req.query.type};`
@@ -310,6 +598,19 @@ app.get('/orderContents', function(req, res)
                 getAndRenderorderContents(req, res);
             }
         });
+    } else if (req.query["crud"] && req.query.crud == 'update') {
+        var contentID = req.query['cid'];
+        if (contentID)
+        {
+            updateorderContent(req, res);
+        }
+        else
+        {
+            // Output an error of some kind.
+            console.error("Missing content ID for update");
+            // For now, just render the page and quietly ignore.
+            getAndRenderorderContents(req, res);
+        }
     } else if (req.query["crud"] && req.query.crud == 'delete') {
         // Delete using cid as the identifier
         let sql = `Delete From OrderContents Where contentID = ${req.query.cid};`
@@ -320,6 +621,7 @@ app.get('/orderContents', function(req, res)
         getAndRenderorderContents(req, res);
     }
 })
+
 /*
     LISTENER
 */
