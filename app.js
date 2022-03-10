f740ebb59e1e329ab4df3ef229b988d1a8b8991f
langet0695 <langet@oregonstate.edu>
2022-03-10 07:44:55 -0800

    updating require db in app.js line 10

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 3050;        
 
-var db = require('./Database/db-connector');         // Set a port number at the top so it's easy to change in the future
+var db = require('./DataBase/db-connector');         // Set a port number at the top so it's easy to change in the future
 
 /*
 console.log(db)
